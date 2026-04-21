995744a9c0780526b55ac2d0249640c2b276f429
Anna-Gladush <ann.gladush@gmail.com>
2026-04-21 17:27:55 +0200

    The 4th time is the charm, eh>:?

--- src/weather-api.js
+++ src/weather-api.js
@@ -2,8 +2,7 @@ const api = '5a4f775d97004809801123746261604';
 
 // Forecast
 async function getWeatherData(city) {
-  const url = `https://api.weatherapi.com/v1/forecast.json?key=${api}&q=${city}&days=3&aqi=no&alerts=no`;
-
+  const url = `https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.weatherapi.com/v1/forecast.json?key=${api}&q=${city}&days=3&aqi=no&alerts=no`)}`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
