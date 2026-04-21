a85b2f1bc7ed28198fd1c710715857c554fbc432
Anna-Gladush <ann.gladush@gmail.com>
2026-04-21 16:48:27 +0200

    Changed url, because of an error

--- src/weather-api.js
+++ src/weather-api.js
@@ -2,7 +2,7 @@ const api = '5a4f775d97004809801123746261604';
 
 // Forecast
 async function getWeatherData(city) {
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${api}&q=${city}&days=3&aqi=no&alerts=no`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${api}&q=${city}&days=3&aqi=no&alerts=no`;
 
   try {
     const response = await fetch(url);
